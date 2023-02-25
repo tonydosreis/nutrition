545e0b6b1b3ace846bebbc0618d2174a92e63926
Tony Reis <tonycdosreis@gmail.com>
2023-02-25 18:07:33 -0500

    Reorganize constraint code into MealSolver class

--- nutrition.py
+++ nutrition.py
@@ -2,6 +2,9 @@ import numpy as np
 from scipy import optimize
 
 class Food():
+
+    ATTR_NAMES = ["cals", "carbs", "proteins", "fats", "quantity"]
+
     def __init__(self, name, unit, serving_size, cals, carbs, proteins, fats):
         self.name = name
         self.unit = unit
@@ -14,7 +17,7 @@ class Food():
         return f"Name: {self.name:>20}, cals: {self.cals:.2f}/g, carbs: {self.carbs:.2f}/g, proteins: {self.proteins:.2f}/g, fats {self.fats:.2f}/g"
 
     def get_attr(self, attr_name):
-        assert attr_name in ["cals", "carbs", "proteins", "fats", "quantity"]
+        assert attr_name in Food.ATTR_NAMES
         if(attr_name == "cals"):
             return self.cals
         elif(attr_name == "carbs"):
@@ -25,52 +28,51 @@ class Food():
             return self.fats
         else:
             return 1
+        
+class MealSolver():
+    def __init__(self, foods):
+        self.foods = foods
+        self.n_foods = len(foods)
+        self.n_equations = 0
+        self.A = []
+        self.B = []
 
-class Constraint():
-    def __init__(self, property, coef, b):
-        assert property in ["cals", "carbs", "proteins", "fats", "quantity"]
-        self.property = property
-        self.coef = np.array(coef)
-        self.b = b
+    def add_equation(self, a,b):
+        assert len(a) == self.n_foods
+        assert type(b) in [float, int]
+        self.A.append(a)
+        self.B.append(b)
+        self.n_equations += 1
 
-class TotalConstraint(Constraint):
-    def __init__(self, foods, property, total):
-        coef = len(foods)*[1]
-        super().__init__(property, coef,total)
+    def add_total_constraint(self, property, quant):
+        assert property in Food.ATTR_NAMES
+        assert quant >= 0
+        a = [food.get_attr(property) for food in self.foods]
+        b = quant
+        self.add_equation(a,b)
 
-class QuantConstraint(Constraint):
-    def __init__(self, foods, food, quant):
-        i = foods.index(food)
-        coef = len(foods)*[0]
-        coef[i]=1
-        super().__init__("quantity", coef, quant)
+    def add_quant_constraint(self, food, quant):
+        assert food in self.foods
+        assert quant >= 0
+        a = [0]*self.n_foods
+        i = self.foods.index(food)
+        a[i] = 1
+        b = quant
+        self.add_equation(a,b)
 
-def meal_prop(foods, constraints):
-    assert type(foods) == list
-    assert type(constraints) == list
-    n_foods = len(foods)
-    n_constraints = len(constraints)
-    
-    A = np.zeros((n_constraints,n_foods))
-    b = np.zeros((n_constraints, 1))
+    def solve(self):
+        assert self.n_equations > 0
+        A = np.array(self.A)
+        B = np.array(self.B).reshape(-1,1)
+        bounds = (0, 1000)
+        x0 =np.random.uniform(low = bounds[0], high = bounds[1], size=(self.n_foods))
+        def fun(x):
+            return (A@x.reshape((-1,1)) - B).flatten()
+        out = optimize.least_squares( fun = fun,x0 = x0, bounds = bounds)
+        quant = out.x
+        res = out.fun
 
-    for i, constraint in enumerate(constraints):
-        A[i,:] = np.array([food.get_attr(constraint.property) for food in foods])*constraint.coef
-        b[i] = constraint.b
-
-    bounds = (0, 1000)
-
-    x0 =np.random.uniform(low = bounds[0], high = bounds[1], size=(n_foods))
-
-    def fun(x):
-        return (A@x.reshape((-1,1)) - b).flatten()
-
-    out = optimize.least_squares( fun = fun,x0 = x0, bounds = bounds)
-
-    quant = out.x
-    res = out.fun
-
-    return quant, res
+        return quant, res
 
 def get_daily_macros(bodyweight, protein_ratio, fat_ratio):
     daily_protein = bodyweight*protein_ratio
@@ -124,3 +126,31 @@ def display_meal(foods, quantity):
 
     print("Macros:")
     print(f"   cals: {calories:.0f}, carbs: {carbs:.0f}g, proteins: {proteins:.0f}g, fats {fats:.0f}g" )
+
+if __name__ =="__main__":
+    rice = Food("Rice", "grams", serving_size=47, cals =170, carbs=37, proteins=3, fats=0)
+    vegetables = Food("Vegetables", "grams", serving_size=100, cals=28, carbs=3, proteins=3, fats=0.5)
+    chicken = Food("Chicken breast", "grams", serving_size=112, cals=100, carbs=0, proteins=24, fats=0.5 )
+    oil = Food("Canola oil", "grams", serving_size=14, cals=120, carbs =0, proteins=0,fats=14)
+
+    foods = [rice, vegetables, chicken, oil]
+
+    bodyweight = 150
+    protein_ratio = 1
+    fat_ratio = .4
+
+    daily_cals = 2200
+    n_meals = 4
+
+    cals_per_meal, protein_per_meal, fats_per_meal = get_macros_per_meal(bodyweight, protein_ratio, fat_ratio, daily_cals, n_meals)
+    display_goal(cals_per_meal, protein_per_meal, fats_per_meal)
+
+    solver = MealSolver(foods)
+    solver.add_total_constraint("proteins", protein_per_meal)
+    solver.add_total_constraint("cals", cals_per_meal)
+    solver.add_total_constraint("fats", fats_per_meal)
+    solver.add_quant_constraint(vegetables, 100)
+
+    quant, res = solver.solve()
+
+    display_meal(foods, quant)
