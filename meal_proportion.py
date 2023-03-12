71ccbefc3240630f2fe51bab49a8c79f30528f4e
Tony Reis <tonycdosreis@gmail.com>
2023-03-12 17:29:08 -0400

    Run formatter and linter

--- meal_proportion.py
+++ meal_proportion.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy import optimize
 
 MIN_QUANT = 0
-MAX_QUANT = 10**4 #10kg
+MAX_QUANT = 10**4  # 10kg
+
 
 class Food():
 
@@ -15,9 +16,14 @@ class Food():
         self.carbs = carbs/serving_size
         self.proteins = proteins/serving_size
         self.fats = fats/serving_size
-    
+
     def __str__(self):
-        return f"Name: {self.name:>20}, cals: {self.cals:.2f}/g, carbs: {self.carbs:.2f}/g, proteins: {self.proteins:.2f}/g, fats {self.fats:.2f}/g"
+        string = (f"Name: {self.name:>20},"
+                  f" cals: {self.cals:.2f}/g,"
+                  f" carbs: {self.carbs:.2f}/g,"
+                  f" proteins: {self.proteins:.2f}/g,"
+                  f" fats {self.fats:.2f}/g")
+        return string
 
     def get_attr(self, attr_name):
         assert attr_name in Food.ATTR_NAMES
@@ -31,17 +37,19 @@ class Food():
             return self.fats
         else:
             return 1
-        
+
+
 class MealSolver():
     def __init__(self, foods):
         self.foods = foods
         self.n_foods = len(foods)
-        self.quant_bounds = [MIN_QUANT*np.ones(self.n_foods), MAX_QUANT*np.ones(self.n_foods)]
+        self.quant_bounds = [
+            MIN_QUANT*np.ones(self.n_foods), MAX_QUANT*np.ones(self.n_foods)]
         self.n_equations = 0
         self.A = []
         self.B = []
 
-    def add_equation(self, a,b):
+    def add_equation(self, a, b):
         assert len(a) == self.n_foods
         assert type(b) in [float, int]
         self.A.append(a)
@@ -53,7 +61,7 @@ class MealSolver():
         assert quant >= 0
         a = [food.get_attr(property) for food in self.foods]
         b = quant
-        self.add_equation(a,b)
+        self.add_equation(a, b)
 
     def add_quant_constraint(self, food, quant):
         assert food in self.foods
@@ -62,9 +70,9 @@ class MealSolver():
         i = self.foods.index(food)
         a[i] = 1
         b = quant
-        self.add_equation(a,b)
+        self.add_equation(a, b)
 
-    def add_bounds(self, food, min = MIN_QUANT, max=MAX_QUANT):
+    def add_bounds(self, food, min=MIN_QUANT, max=MAX_QUANT):
         assert food in self.foods
         assert min >= MIN_QUANT and max <= MAX_QUANT
         i = self.foods.index(food)
@@ -74,16 +82,19 @@ class MealSolver():
     def solve(self):
         assert self.n_equations > 0
         A = np.array(self.A)
-        B = np.array(self.B).reshape(-1,1)
+        B = np.array(self.B).reshape(-1, 1)
         bounds = self.quant_bounds
-        x0 =np.random.uniform(low = bounds[0], high = bounds[1], size=(self.n_foods))
+        x0 = np.random.uniform(
+            low=bounds[0], high=bounds[1], size=(self.n_foods))
+
         def fun(x):
-            return (A@x.reshape((-1,1)) - B).flatten()
-        out = optimize.least_squares( fun = fun,x0 = x0, bounds = bounds)
+            return (A@x.reshape((-1, 1)) - B).flatten()
+        out = optimize.least_squares(fun=fun, x0=x0, bounds=bounds)
         quant = out.x
         res = out.fun
 
         return quant, res
+
 
 def get_daily_macros(bodyweight, protein_ratio, fat_ratio):
     daily_protein = bodyweight*protein_ratio
@@ -91,9 +102,11 @@ def get_daily_macros(bodyweight, protein_ratio, fat_ratio):
 
     return daily_protein, daily_fats
 
-def get_macros_per_meal(bodyweight, protein_ratio, fat_ratio, daily_cals, n_meals):
-    """Calculates the macros for each meal in a day.
-    
+
+def get_macros_per_meal(bodyweight, protein_ratio,
+                        fat_ratio, daily_cals, n_meals):
+    """Calculate the macros for each meal in a day.
+
     Recommended values are for a gaining phase.
 
     Args:
@@ -108,8 +121,8 @@ def get_macros_per_meal(bodyweight, protein_ratio, fat_ratio, daily_cals, n_meal
         protein_per_meal
         fats_per_meal
     """
-
-    daily_protein, daily_fats = get_daily_macros(bodyweight, protein_ratio, fat_ratio)
+    daily_protein, daily_fats = get_daily_macros(
+        bodyweight, protein_ratio, fat_ratio)
 
     cals_per_meal = daily_cals/n_meals
     protein_per_meal = daily_protein/n_meals
@@ -117,9 +130,14 @@ def get_macros_per_meal(bodyweight, protein_ratio, fat_ratio, daily_cals, n_meal
 
     return cals_per_meal, protein_per_meal, fats_per_meal
 
+
 def display_goal(cals_per_meal, protein_per_meal, fats_per_meal):
     print("Goal:")
-    print(f"   cals: {cals_per_meal:.0f}, proteins: {protein_per_meal:.0f}g, fats {fats_per_meal:.0f}g" )
+    string = (f"  cals: {cals_per_meal:.0f}, "
+              f"proteins: {protein_per_meal:.0f}g, "
+              f"fats {fats_per_meal:.0f}g")
+    print(string)
+
 
 def display_meal(foods, quantity):
     calories = 0
@@ -136,13 +154,21 @@ def display_meal(foods, quantity):
         fats += food.fats*quant
 
     print("Macros:")
-    print(f"   cals: {calories:.0f}, carbs: {carbs:.0f}g, proteins: {proteins:.0f}g, fats {fats:.0f}g" )
+    print(f"cals: {calories:.0f}, "
+          f"carbs: {carbs:.0f}g, "
+          f"proteins: {proteins:.0f}g, "
+          f"fats {fats:.0f}g")
 
-if __name__ =="__main__":
-    rice = Food("Rice", "grams", serving_size=47, cals =170, carbs=37, proteins=3, fats=0)
-    vegetables = Food("Vegetables", "grams", serving_size=100, cals=28, carbs=3, proteins=3, fats=0.5)
-    chicken = Food("Chicken breast", "grams", serving_size=112, cals=100, carbs=0, proteins=24, fats=0.5 )
-    oil = Food("Canola oil", "grams", serving_size=14, cals=120, carbs =0, proteins=0,fats=14)
+
+if __name__ == "__main__":
+    rice = Food("Rice", "grams", serving_size=47,
+                cals=170, carbs=37, proteins=3, fats=0)
+    vegetables = Food("Vegetables", "grams", serving_size=100,
+                      cals=28, carbs=3, proteins=3, fats=0.5)
+    chicken = Food("Chicken breast", "grams", serving_size=112,
+                   cals=100, carbs=0, proteins=24, fats=0.5)
+    oil = Food("Canola oil", "grams", serving_size=14,
+               cals=120, carbs=0, proteins=0, fats=14)
 
     foods = [rice, vegetables, chicken, oil]
 
@@ -153,14 +179,15 @@ if __name__ =="__main__":
     daily_cals = 2200
     n_meals = 4
 
-    cals_per_meal, protein_per_meal, fats_per_meal = get_macros_per_meal(bodyweight, protein_ratio, fat_ratio, daily_cals, n_meals)
+    cals_per_meal, protein_per_meal, fats_per_meal = get_macros_per_meal(
+        bodyweight, protein_ratio, fat_ratio, daily_cals, n_meals)
     display_goal(cals_per_meal, protein_per_meal, fats_per_meal)
 
     solver = MealSolver(foods)
     solver.add_total_constraint("proteins", protein_per_meal)
     solver.add_total_constraint("cals", cals_per_meal)
     solver.add_total_constraint("fats", fats_per_meal)
-    solver.add_bounds(vegetables, min = 80, max = 120)
+    solver.add_bounds(vegetables, min=80, max=120)
 
     quant, res = solver.solve()
 
--- meal_timing.py
+++ meal_timing.py
@@ -1,36 +1,48 @@
 import numpy as np
 from scipy import optimize
 
+
 def dec2time(x):
     hour = int(x)
     minutes = int((300/5)*(x - hour))
 
-    if(hour == 0): return f"{minutes:0>2}m"
-    else: return f"{hour:0>2}:{minutes:0>2}h"
+    if(hour == 0):
+        return f"{minutes:0>2}m"
+    else:
+        return f"{hour:0>2}:{minutes:0>2}h"
 
-def calc_meal_time_old(t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep):
-    A = np.array([[-1,2,-1,0],[0 , -1, 2, -1],[1,0,0,0],[0,0,1,0],[0,0,0,1]])
-    b = np.array([0,0,t_wake + delta_wake, t_gym - delta_gym, t_sleep - delta_sleep]).reshape((-1,1))
+
+def calc_meal_time_old(t_wake, delta_wake, t_gym,
+                       delta_gym, t_sleep, delta_sleep):
+    A = np.array([[-1, 2, -1, 0], [0, -1, 2, -1],
+                 [1, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    b = np.array([0, 0, t_wake + delta_wake, t_gym - delta_gym,
+                 t_sleep - delta_sleep]).reshape((-1, 1))
 
     n = A.shape[1]
+
     def fun(x):
-        x = x.reshape((-1,1))
+        x = x.reshape((-1, 1))
         return (A@x - b).flatten()
 
-    bounds = ([t_wake + delta_wake,t_wake,t_wake,t_wake],[t_sleep,t_sleep,t_sleep,t_sleep])
+    bounds = ([t_wake + delta_wake, t_wake, t_wake, t_wake],
+              [t_sleep, t_sleep, t_sleep, t_sleep])
 
-    res = optimize.least_squares(fun, np.random.uniform(low = bounds[0], high = bounds[1], size=(n)), bounds = bounds)
+    res = optimize.least_squares(fun, np.random.uniform(
+        low=bounds[0], high=bounds[1], size=(n)), bounds=bounds)
     m = res.x
 
     return m
 
-def calc_meal_time(t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep, n_meals):
-    """
-    Calculates meal times trying to have equal meal intervals and considering some desired parameters.
 
-    The desired time for first meal is t_wake + delta_wake, for the pregym meal is t_gym - delta_gym and
-    for the last meal t_sleep - delta_sleep. The solution tries to balance having equal intervals and also
-    being close to the desired times.
+def calc_meal_time(t_wake, delta_wake, t_gym, delta_gym,
+                   t_sleep, delta_sleep, n_meals):
+    """Calculate meal times trying to have equal intervals and other criteria.
+
+    The desired time for first meal is t_wake + delta_wake, for the pregym meal
+    is t_gym - delta_gym and for the last meal t_sleep - delta_sleep. The
+    solution tries to balance having equal intervals and also being close to
+    the desired times.
 
     Args:
         t_wake: wake up time.
@@ -46,26 +58,30 @@ def calc_meal_time(t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep, n_
 
     assert n_meals >= 3
 
-    #Create A matrix
+    # Create A matrix
     A = []
-    #Equal intervals constraints
+    # Equal intervals constraints
     for i in range(n_meals - 2):
         a_row = []
         for j in range(n_meals):
-            if( (j == i) or (j == i + 2) ):  a_row.append(-1)
-            elif( j == i + 1 ): a_row.append(2)
-            else: a_row.append(0)
+            if((j == i) or (j == i + 2)):
+                a_row.append(-1)
+            elif(j == i + 1):
+                a_row.append(2)
+            else:
+                a_row.append(0)
         A.append(a_row)
 
-    #Desired time constraints
+    # Desired time constraints
     a_row = n_meals*[0]
     a_row[0] = 1
     A.append(a_row)
 
-    #Aproximation of which meal will be the pregym meal
-    pre_gym_meal = int((t_gym - delta_gym-t_wake - delta_sleep)/((t_sleep - delta_sleep - t_wake - delta_sleep)/n_meals))
+    # Approximation of which meal will be the pregym meal
+    pregym_meal = int((t_gym - delta_gym-t_wake - delta_sleep) /
+                      ((t_sleep - delta_sleep - t_wake - delta_sleep)/n_meals))
     a_row = n_meals*[0]
-    a_row[pre_gym_meal] = 1
+    a_row[pregym_meal] = 1
     A.append(a_row)
 
     a_row = n_meals*[0]
@@ -74,32 +90,36 @@ def calc_meal_time(t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep, n_
 
     A = np.array(A)
 
-    #Create b
+    # Create b
     b = (1+n_meals)*[0]
     b[-1] = t_sleep - delta_sleep
     b[-2] = t_gym - delta_gym
     b[-3] = t_wake + delta_wake
 
-    b = np.array(b).reshape((-1,1))
+    b = np.array(b).reshape((-1, 1))
 
-    #Bounds
+    # Bounds
     lb = n_meals*[t_wake]
     lb[0] += delta_wake
     ub = n_meals*[t_sleep]
-    bounds = (lb,ub)
+    bounds = (lb, ub)
 
     n = A.shape[1]
+
     def fun(x):
-        x = x.reshape((-1,1))
+        x = x.reshape((-1, 1))
         return (A@x - b).flatten()
 
-    res = optimize.least_squares(fun, np.random.uniform(low = bounds[0], high = bounds[1], size=(n)), bounds = bounds)
+    res = optimize.least_squares(fun, np.random.uniform(
+        low=bounds[0], high=bounds[1], size=(n)), bounds=bounds)
     m = res.x
 
     return m
 
-def display_meal_time(m, t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep):
-    """Formats and displays useful information based on return from cal_meal_time()"""
+
+def display_meal_time(m, t_wake, delta_wake, t_gym,
+                      delta_gym, t_sleep, delta_sleep):
+    """Format and display useful information returned by cal_meal_time"""
 
     print("Meal Times:")
     for i, time_dec in enumerate((m)):
@@ -107,19 +127,24 @@ def display_meal_time(m, t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sle
 
     print()
 
-    print("Meal intervals:", end = " ")
-    print(f"{[ dec2time(delta) for delta in np.diff(m)]}, std = {dec2time(np.std(np.diff(m)))}")
+    print("Meal intervals:", end=" ")
+    print(f"{[ dec2time(delta) for delta in np.diff(m)]}, "
+          f"std = {dec2time(np.std(np.diff(m)))}")
 
     print("Pregym meal interval:")
 
     pregym_m_index = np.argmax(m > t_gym) - 1
-    print(f"\tDesired = {dec2time(delta_gym)}, current = {dec2time(t_gym - m[pregym_m_index])}")
+    print(f"\tDesired = {dec2time(delta_gym)}, "
+          f"current = {dec2time(t_gym - m[pregym_m_index])}")
 
     print("Presleep meal interval:")
-    print(f"\tDesired = {dec2time(delta_sleep)}, current = {dec2time(t_sleep - m[-1])}")
+    print(f"\tDesired = {dec2time(delta_sleep)}, "
+          f"current = {dec2time(t_sleep - m[-1])}")
 
     print("Postwake meal interval:")
-    print(f"\tDesired = {dec2time(delta_wake)}, current = {dec2time(m[0] - t_wake)}")
+    print(f"\tDesired={dec2time(delta_wake)}, "
+          f"current = {dec2time(m[0] - t_wake)}")
+
 
 if __name__ == "__main__":
     t_wake = 8
@@ -130,5 +155,7 @@ if __name__ == "__main__":
     delta_sleep = 3
     n_meals = 4
 
-    m = calc_meal_time(t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep, n_meals)
-    display_meal_time(m, t_wake,delta_wake, t_gym, delta_gym, t_sleep, delta_sleep)
+    m = calc_meal_time(t_wake, delta_wake, t_gym, delta_gym,
+                       t_sleep, delta_sleep, n_meals)
+    display_meal_time(m, t_wake, delta_wake, t_gym,
+                      delta_gym, t_sleep, delta_sleep)
